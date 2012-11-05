fc00f5610e6d359a079fe058dbb9e88e668e4d4c
Anoop Hallur <anooprh@thoughtworks.com>
2012-11-05 11:58:50 -0500

    Script Ready to be used

--- download.sh
+++ download.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-NO_OF_ITEMS=3
-for i in {1..$NO_OF_ITEMS}
+echo Installing Home Brew...
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+echo Installing wget ..
+brew install wget
+sudo apt-get install wget
+NO_OF_ITEMS=1426
+DIR=$(cd "$(dirname "$0")"; pwd)
+echo $NO_OF_ITEMS must by downloaded..Saving to $DIR
+for ((i = 1 ; i <= $NO_OF_ITEMS ; i++))
 do
-wget --recursive --level=1 --span-hosts --tries=1 --no-directories --timestamping --no-parent --execute="robots=off" --verbose --continue --directory-prefix=/home/anoop/Downloads/WgetDownlds http://link.songspk.info/revival_old-is-gold/download/revival.php?id=$i
+	echo Item $i is being fetched..
+	wget --recursive --level=1 --span-hosts --tries=1 --no-directories --timestamping --no-parent --execute="robots=off" --verbose --continue --directory-prefix=$DIR http://link.songspk.info/revival_old-is-gold/download/revival.php?id=$i
 done
