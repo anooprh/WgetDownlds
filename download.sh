10553403a15000c51fe29576a16c706ade31a333
Anoop Hallur <anooprh@thoughtworks.com>
2012-11-05 13:08:43 -0500

    Added 30 minsTime out after 20 items to prevent IP Blocking

--- download.sh
+++ download.sh
@@ -3,7 +3,6 @@ echo Installing Home Brew...
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 echo Installing wget ..
 brew install wget
-sudo apt-get install wget
 NO_OF_ITEMS=1426
 DIR=$(cd "$(dirname "$0")"; pwd)
 echo $NO_OF_ITEMS must by downloaded..Saving to $DIR
@@ -11,4 +10,8 @@ for ((i = 1 ; i <= $NO_OF_ITEMS ; i++))
 do
 	echo Item $i is being fetched..
 	wget --recursive --level=1 --span-hosts --tries=1 --no-directories --timestamping --no-parent --execute="robots=off" --verbose --continue --directory-prefix=$DIR http://link.songspk.info/revival_old-is-gold/download/revival.php?id=$i
+	
+	if[ $(($i%20)) -eq 0 ]
+			echo 20 Items have been downloaded.. System going to sleep about 30 mins..Take a Break..
+			sleep 1800
 done
